5b91dcf1800e843fef2c8045380bf70becd73a36
Gabriel <gabrielpbenedicto@gmail.com>
2021-12-31 21:26:38 -0300

    Preparação do projeto

---
+++ front/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { BrowserRouter, Router, Route } from 'react-dom';
+
+function App() {
+  return (
+    <div className="App">
+    <BrowserRouter>
+      <Router>
+        <Route path='' component=''/>
+      </Router>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
