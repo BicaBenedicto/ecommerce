b6eeab33eac0234d19834cecd3992cd34e382f89
Gabriel <gabrielpbenedicto@gmail.com>
2022-01-01 14:33:16 -0300

    Testes homepage

--- front/src/tests/home.test.js
+++ front/src/tests/home.test.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import App from '../App';
+
+const EMAIL = 'exemplo@email.com';
+const WRONG_EMAIL = 'abc@email';
+const WRONG_EMAIL_2 = 'abcemail.com';
+const WRONG_EMAIL_3 = 'abcemail@.com';
+const PASSWORD_SEARCH = '******'
+const PASSWORD = 'abc123'
+const WRONG_PASSWORD = 'abc12';
 
 describe('verify homepage have correct elements and actions expected', () => {
   it('have login inputs', () => {
     render(<App />);
 
-    const [inputEmail] = screen.queryAllByPlaceholderText('exemplo@email.com', 'input');
-    const [inputPassword] = screen.queryAllByPlaceholderText('********', 'input');
+    const [inputEmail] = screen.queryAllByPlaceholderText(EMAIL, 'input');
+    const [inputPassword] = screen.queryAllByPlaceholderText(PASSWORD_SEARCH, 'input');
 
     expect(inputEmail).toBeInTheDocument();
     expect(inputPassword).toBeInTheDocument();
@@ -24,5 +32,63 @@ describe('verify homepage have correct elements and actions expected', () => {
 
     expect(logo).toHaveAttribute('src', /logo/i);
     expect(logo).toHaveAttribute('alt', /logo/i);
+  });
+
+  it('have verify in email and password', () => {
+    render(<App />);
+
+    const [inputEmail] = screen.queryAllByPlaceholderText(EMAIL, 'input');
+    const [inputPassword] = screen.queryAllByPlaceholderText(PASSWORD_SEARCH, 'input');
+    const button = screen.getByRole('button', { name: 'Entrar' });
+
+    expect(button).toBeDisabled();
+
+    fireEvent.change(inputEmail, { target: { value: WRONG_EMAIL }});
+
+    expect(button).toBeDisabled();
+
+    fireEvent.change(inputPassword, { target: { value: PASSWORD }});
+
+    expect(button).toBeDisabled();
+
+    fireEvent.change(inputEmail, { target: { value: WRONG_EMAIL_2 }});
+
+    expect(button).toBeDisabled();
+
+    fireEvent.change(inputEmail, { target: { value: WRONG_EMAIL_3 }});
+
+    expect(button).toBeDisabled();
+
+    fireEvent.change(inputPassword, { target: { value: WRONG_PASSWORD }});
+
+    expect(button).toBeDisabled();
+
+    fireEvent.change(inputEmail, { target: { value: EMAIL }});
+
+    expect(button).toBeDisabled();
+
+    fireEvent.change(inputPassword, { target: { value: PASSWORD }});
+
+    expect(button).not.toBeDisabled();
+  });
+
+  it('redirect in login button', () => {
+    render(<App />);
+
+    const [inputEmail] = screen.queryAllByPlaceholderText(EMAIL, 'input');
+    const [inputPassword] = screen.queryAllByPlaceholderText(PASSWORD_SEARCH, 'input');
+    const button = screen.getByRole('button', { name: 'Entrar' });
+
+    expect(button).toBeDisabled();
+
+    fireEvent.change(inputEmail, { target: { value: EMAIL }});
+    fireEvent.change(inputPassword, { target: { value: PASSWORD }});
+
+    expect(button).not.toBeDisabled();
+
+    fireEvent.click(button);
+
+    expect(inputEmail).not.toBeInTheDocument();
+    expect(inputPassword).not.toBeInTheDocument();
   })
 })
