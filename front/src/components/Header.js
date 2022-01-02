46ce2ff4b823bd1aef42e6b04c58660679570673
Gabriel <gabrielpbenedicto@gmail.com>
2022-01-02 00:02:59 -0300

    Iniciado página produtos

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../imgs/logo.png';
+import '../css/Header.css';
 
 export default function Header() {
   return (
--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import Context from '../services/Context';
+import userIcon from '../imgs/icons/user-icon.svg';
+import lockIcon from '../imgs/icons/lock-icon.svg';
 
 export default function Login() {
   const navigate = useNavigate();
@@ -33,20 +35,26 @@ export default function Login() {
 
   return (
     <form onSubmit={ handleSubmit}>
-      <input
-        type="email"
-        placeholder='exemplo@email.com'
-        value={ email }
-        name="email"
-        onChange={ ({ target }) => setEmail(target.value) }
-      />
-      <input
-        type="password"
-        placeholder='******'
-        value={ password }
-        name="password"
-        onChange={ ({ target }) => setPassword(target.value) }
-      />
+      <label>
+        <img src={ userIcon } alt="user-icon" className='icon-inputs' />
+        <input
+          type="email"
+          placeholder='exemplo@email.com'
+          value={ email }
+          name="email"
+          onChange={ ({ target }) => setEmail(target.value) }
+        />
+      </label>
+      <label>
+        <img src={ lockIcon } alt="lock-icon" className='icon-inputs' />
+        <input
+          type="password"
+          placeholder='******'
+          value={ password }
+          name="password"
+          onChange={ ({ target }) => setPassword(target.value) }
+        />
+      </label>
       <button
         type="submit"
         disabled={ hasDisabled }
